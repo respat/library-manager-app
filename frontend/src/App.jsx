45c9423321509a24907616c0184440e9a68b66c8
respat <respergerpatrik@gmail.com>
2023-11-05 08:35:06 +0100

    Layout változtatás, feltöltés gomb hozzáadása

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Card from './components/Card'
 import Upload from './components/Upload'
+import ListItem from './components/ListItem'
 
 function App() {
 
@@ -10,9 +11,11 @@ function App() {
       <div className='bg-slate-50 w-screen h-screen'>
         <Navbar />
         <div className="flex items-center justify-center">
-        <Upload />
-        </div>
+          <Card />        </div>
+          <div className="flex items-center justify-center mt-20">
+          <Upload />        </div>
       </div>
+      
   )
 }
 
